ebcf14596e6e3d1d32d18af5e6e76d3bd508ce61
monke-yo <monkeysingh100@gmail.com>
2024-11-29 13:44:26 +0530

    fixed options bug

--- content.js
+++ content.js
@@ -19,7 +19,9 @@ document.addEventListener("keydown", () => {
 
 // Function to play the sound
 function playKeySound() {
-  const audio = new Audio(chrome.runtime.getURL(`sounds/click.mp3`));
+  const audio = new Audio(
+    chrome.runtime.getURL(`sounds/${soundSettings.soundFile}`)
+  );
   audio.volume = soundSettings.volume;
   audio.play().catch((err) => console.error("Error playing sound:", err));
 }
